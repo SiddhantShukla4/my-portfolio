1e75f36a152df913c2e667719660315d19ba0a11
SiddhantShukla4 <siddhantshukla206@gmail.com>
2025-05-06 01:18:05 +0530

    update

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -13,7 +13,7 @@ const About = () => {
               
               <div className="relative z-10 rounded-lg overflow-hidden shadow-xl">
                 <img 
-                  src="/utilities/images/hacked.jpg" 
+                  src="/src/utilities/images/hacked.jpg" 
                   alt="Developer working on code" 
                   className="w-full h-full object-cover"
                 />
